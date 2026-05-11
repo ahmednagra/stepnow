afd12b7124f0172cb24d87f24a5e922914b7c6ee
ahmednagra <ahmednagra9@gmail.com>
2026-05-11 22:17:57 +0500

    files removed

--- apps/frontend/src/src/components/features/home/index.ts
+++
@@ -1,8 +0,0 @@
-// src/components/features/home/index.ts
-export { TrustStrip } from "./TrustStrip";
-export { HowItWorks } from "./HowItWorks";
-export { WhyStepNow } from "./WhyStepNow";
-export { FleetPreview } from "./FleetPreview";
-export { TestimonialsSection } from "./TestimonialsSection";
-export { FaqTeaser } from "./FaqTeaser";
-export { HeroFeatureBlock } from "./HeroFeatureBlock";
